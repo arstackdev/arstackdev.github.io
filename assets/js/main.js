7dcd867431088f72c3aeb5020d47cfdc7847b628
Abdul Rehman <arstackdev.io@gmail.com>
2026-03-07 20:39:03 +0500

    Header modified with new design

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 50);
 });
+
+
+/* ----- MENU TOGGLE ----- */
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", function () {
+    nav.classList.toggle("active");
+});
